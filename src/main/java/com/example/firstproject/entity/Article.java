6c4c9876fa6226f061cf57573906f1102b7e8f1f
lecielpourlamer <shlim20190902@gmail.com>
2023-08-30 13:14:10 +0900

    chapter5. 5-2 완료

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,14 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @Entity // 엔티티 선언
 public class Article {
-    public Article() {
-    }
 
     @Id // 엔티티의 대푯값 지정
     @GeneratedValue // 자동 생성 기능 추가(숫자가 자동으로 매겨짐)
