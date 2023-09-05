e038b9f67166777a6a09f2731ccf2300f754b9a5
lecielpourlamer <shlim20190902@gmail.com>
2023-09-06 08:52:45 +0900

    chapter9 완료 / Part 2 완료

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +14,7 @@ import lombok.ToString;
 public class Article {
 
     @Id // 엔티티의 대푯값 지정
-    @GeneratedValue // 자동 생성 기능 추가(숫자가 자동으로 매겨짐)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 id 자동 생성, 자동 생성 기능 추가(숫자가 자동으로 매겨짐)
     private Long id;
 
     @Column // title 필드 선언, DB 테이블에 title 열과 연결됨
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
-INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
+INSERT INTO article(title, content) VALUES('가가가가', '1111');
+INSERT INTO article(title, content) VALUES('나나나나', '2222');
+INSERT INTO article(title, content) VALUES('다다다다', '3333');
 
 INSERT INTO member(id, email, password) VALUES(1, 'mail@cloudstudying.kr', '1111');
 INSERT INTO member(id, email, password) VALUES(2, 'sehongpark@cloudstudying.kr', '1111');
